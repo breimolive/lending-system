753f58ca517944d7abb958789ea3bae10e3bcf16
Heine breimo <heine.breimo@tietoevry.com>
2026-06-17 11:04:19 +0200

    rest

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,40 +1,40 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Server.Database;
+using Server.Exceptions;
+using Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers()
+    .AddJsonOptions(options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); });
 
-// Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddProblemDetails();
 
+builder.Services.AddScoped<EquipmentService>();
+builder.Services.AddScoped<LoanService>();
+builder.Services.AddScoped<UserService>();
+builder.Services.AddHttpContextAccessor();
 
-// Add Authentication
 builder.Services
     .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Create and migrate database
-using (var serviceScope = app.Services.CreateScope())
-{
-    var context = serviceScope.ServiceProvider.GetRequiredService<DatabaseContext>();
-    context.Database.Migrate();
-}
+await DatabaseContextSeed.SeedAsync(app.Services);
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -44,8 +44,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 app.UseAuthorization();
 
-
-
+app.UseExceptionHandler();
 app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
