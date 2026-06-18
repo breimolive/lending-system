3d6e0d66466b8e7d13edc802e1f93e14185c4f3a
Heine breimo <heine.breimo@tietoevry.com>
2026-06-18 12:59:33 +0200

    Implemented: Equipment page

---
+++ Client/src/app/equipment/equipment.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-equipment',
+  standalone: true,
+  templateUrl: './equipment.component.html',
+  styleUrl: './equipment.component.css'
+})
+export class EquipmentComponent {
+
+}
